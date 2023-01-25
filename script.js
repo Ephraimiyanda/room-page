cde748e7465d118c8995f58638271ce6e1ce06e9
Ephraim Iyanda <113598104+Ephraimiyanda@users.noreply.github.com>
2023-01-25 12:15:37 -0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const backwards = document.querySelector(".previous");
 const further = document.querySelector(".next");
 const retro = document.querySelector(".retro");
+
 var images = ["desktop-image-hero-1.jpg","desktop-image-hero-2.jpg","desktop-image-hero-3.jpg"];
 var i = 0;
+
+let article = document.querySelectorAll('.first');
+let index = 0;
+
 function setImg(){
     return retro.setAttribute('src', 'images/'+images[i]);
 };
 
 function previous(){
+    article[index].classList.remove('active');
+    index = (index - 1 + article.length)% article.length;
+    article[index].classList.add('active');
+
     if(i <= 0)i =images.length;
     i--;
     return setImg();
@@ -15,24 +24,16 @@ function previous(){
 backwards.addEventListener("click", previous)
 
 function next(){
+    
+article[index].classList.remove('active');
+index = (index + 1)% article.length;
+article[index].classList.add('active');
+
 if(i >= images.length -1)i = -1;
 i++;
 return setImg();
 
+
 }
 further.addEventListener("click", next);
-var firstDiv = 0;
-function show(){
-    $(".firstp").hide();
-    $(".firstp:eq("+ firstDiv +")").show();
-}
-show()
-function nextparagraph(){
-if(firstDiv==$(".firstp").length-1){
-    firstDiv = 0;
-}else{
-    firstDiv ++;
-}
-show()
-}
-ligth.addEventListener("click", nextparagraph);
+
