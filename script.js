8101aa3e6a27ebc6fb99dfac1aa3f4f96d22607b
Ephraim Iyanda <113598104+Ephraimiyanda@users.noreply.github.com>
2023-03-18 23:40:07 +0100

    corrected mistakes

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
 const backwards = document.querySelector(".previous");
 const further = document.querySelector(".next");
-const retro = document.querySelector(".retro");
-var burger = document.querySelector(".burger");
-var list =document.querySelector(".unordered");
-var close = document.querySelector(".close");
+const burger=document.querySelector(".burger");
+const links = document.querySelector(".navLinks");
+const cross = document.querySelector(".close");
+const room = document.querySelector(".room");
 
-var images = ["desktop-image-hero-1.jpg","desktop-image-hero-2.jpg","desktop-image-hero-3.jpg"];
-var i = 0;
+function displaybar(){
+    links.classList.remove("hidden");
+    burger.style.display="none";
+    room.style.display="none";
+}
+function closebar(){
+  links.classList.add("hidden");
+  burger.style.display="block";
+  room.style.display="flex";
+}
+burger.addEventListener("click", displaybar);
+cross.addEventListener("click", closebar );
 
 let article = document.querySelectorAll('.first');
 let index = 0;
 
-function setImg(){
-    return retro.setAttribute('src', 'images/'+images[i]);
-};
+
 
 function previous(){
     article[index].classList.remove('active');
     index = (index - 1 + article.length)% article.length;
     article[index].classList.add('active');
 
-    if(i <= 0)i =images.length;
-    i--;
-    return setImg();
 }
 backwards.addEventListener("click", previous)
 
+
+
 function next(){
-    
 article[index].classList.remove('active');
 index = (index + 1)% article.length;
 article[index].classList.add('active');
-
-if(i >= images.length -1)i = -1;
-i++;
-return setImg();
-
 
 }
 further.addEventListener("click", next);
 
 
-function displayList(){
-    list.classList.remove("base")
-}
-function closeList(){
-    list.classList.add("base")
-}
-burger.addEventListener("click", displayList);
-close.addEventListener("click", closeList);
+
+
+
 
